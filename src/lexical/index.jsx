411694e8267d8d19685b222fd0ca6e0e550d499b
huang yao <kirayao@hotmail.com>
2022-11-12 17:57:25 +0800

    feat: init project

---
+++ src/lexical/index.jsx
@@ -0,0 +1,9 @@
+import "./main/setupEnv";
+import "./main/index.css";
+
+import Main from "./main/App";
+
+export default function LexicalEditor() {
+  // return <div>lexical</div>;
+  return <Main />;
+}
