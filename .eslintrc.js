f74b32639fd2408f27564f42acf2b51a09bf2910
huang yao <kirayao@hotmail.com>
2022-11-20 16:32:09 +0800

    refactor: add eslint add move to packages

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  env: {
+    es2022: true,
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  globals: {},
+  overrides: [
+    {
+      files: ["packages/**.js", "packages/**.jsx"],
+      parser: "babel-eslint",
+      rules: {
+        "react-hooks/exhaustive-deps": "off",
+      },
+    },
+    {
+      files: ["packages/**.tsx", "packages/**.ts"],
+      parser: "@typescript-eslint/parser",
+      rules: {},
+    },
+  ],
+};
--- packages/simple-playground/src/lexical/index.jsx
+++ packages/simple-playground/src/lexical/index.jsx
@@ -1,6 +1,6 @@
 import "./main/index.css";
 
-import Main from "./main";
+import Main from "./main/index.tsx";
 
 export default function LexicalEditor({ ...rest }) {
   return <Main {...rest} />;
